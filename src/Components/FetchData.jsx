22c2e12435089471684bc012fa98ac366bc03834
arun prajapati <arun.p@knackroot.com>
2023-06-04 15:17:17 +0530

    fix fetch

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,27 +1,41 @@
 import { useState } from "react";
 import useDataFetching from "./hooks/useDataFetching";
-const url = `https://api.github.com/users/`;
-const FetchData=() => {
-	const [input, setInput]=useState("");
-  const { data, loading, error }=useDataFetching( url);
-  {
-	  loading && <h1>Loading...</h1>;
+
+const url = "https://api.github.com/users/";
+
+const FetchData = () => {
+	const [input, setInput] = useState("");
+	const { data, loading, error } = useDataFetching(`${url}${input}`); // Append the input value to the API URL to fetch data for a specific user
+
+	if (loading) {
+		return <h1>Loading...</h1>; 
 	}
-	{
-		error && <h1>Error Occurred...</h1>;
+
+	if (error) {
+		return <h1>Error Occurred...</h1>; 
 	}
-  return (
+	return (
 		<div>
 			<h1>useDataFetching demo</h1>
 			<div className="fixed top-0 left-0 right-0 p-4 text-center bg-green-200 rounded-lg shadow-lg">
-				<input type="text" />
-				<button
-					onClick={(e) => {
+				<input
+					type="text"
+					value={input}
+					onChange={(e) => {
 						setInput(e.target.value);
 					}}
-				>
-					Get Data
-				</button>
+				/>{" "}
+				<button onClick={() => setInput("")}>Clear</button>{" "}
+				<button>Get Data</button>
+				{data && ( 
+					<div key={data.id}>
+						<p>Name:{data.name}</p>
+						<p>Bio:{data.bio}</p>
+						<p>Location:{data.location}</p>
+						<p>public Repo:{data.public_repos} Public Repositories</p>
+						<p>Followers:{data.followers} Followers</p>
+					</div>
+				)}
 			</div>
 		</div>
 	);
